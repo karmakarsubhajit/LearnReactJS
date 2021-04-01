9879ad92aee48cebf5b9d47909066e3ff1c43b8c
111801041 <111801041@smail.iitpkd.ac.in>
2021-04-01 13:41:53 +0530

    Commiting Move components

---
+++ Files/my-app/src/components/MyInfo.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+
+function MyInfo()
+{
+    return (
+        <div>
+            <h1>
+                Subhajit Karmakar
+            </h1>    
+            <p>
+                Seeking Antifragility | Polyglot
+            </p>
+            <ol>
+                <li>Barjedov</li>
+                <li>Paris</li>
+                <li>New York</li>
+            </ol>
+            <ol>
+                <li>Lyon</li>
+                <li>Montreal</li>
+                <li>Berlin</li>
+            </ol>
+        </div>
+    )
+}
+
+
+export default MyInfo
--- Files/my-app/src/index.js
+++ Files/my-app/src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-ReactDOM.render(<div><h1>Hello World :)</h1> <h2>Bye World! :(</h2></div>,document.getElementById("root"))
+import MyInfo from "./components/MyInfo"
 
 
+ReactDOM.render(<MyInfo/>,document.getElementById("root"));
