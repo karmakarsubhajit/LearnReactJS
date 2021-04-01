303593e3d78c8fdbe5608cdc00c4bf89fd5a4b69
111801041 <111801041@smail.iitpkd.ac.in>
2021-04-01 15:27:06 +0530

    Todo App Phase 1

--- Files/my-app/src/App.js
+++ Files/my-app/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App()
+{
+    return (
+    <div> 
+      <input type="checkbox" id ="item1" value ="v1" name="n1"/>
+      <label for="item1"><p>This is my first item</p></label>
+      
+      <input type="checkbox" id ="item2" value ="v2" name="n2"/>
+      <label for="item2"><p>This is my second item</p></label>
+      
+      <input type="checkbox" id ="item3" value ="v3" name="n3"/>
+      <label for="item3"><p>This is my third item</p></label>
+      
+      <input type="checkbox" id ="item4" value ="v4" name="n4"/>
+      <label for="item4"><p>This is my fourth item</p></label>
+  
     </div>
-  );
+    )
+
+
+
 }
 
-export default App;
+
+export default App
