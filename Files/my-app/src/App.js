6eaa3b5b360ff7597986a0f05dd48ab734ee8f0e
111801041 <subhajitkarmakar22@gmail.com>
2021-04-02 10:34:21 +0530

    Phase two of TodoList

--- Files/my-app/src/App.js
+++ Files/my-app/src/App.js
@@ -1,26 +1,18 @@
 import React from "react"
+import Todoitem from './components/Todoitem'
+import './index.css'
+
 
 function App()
 {
     return (
-    <div> 
-      <input type="checkbox" id ="item1" value ="v1" name="n1"/>
-      <label for="item1"><p>This is my first item</p></label>
-      
-      <input type="checkbox" id ="item2" value ="v2" name="n2"/>
-      <label for="item2"><p>This is my second item</p></label>
-      
-      <input type="checkbox" id ="item3" value ="v3" name="n3"/>
-      <label for="item3"><p>This is my third item</p></label>
-      
-      <input type="checkbox" id ="item4" value ="v4" name="n4"/>
-      <label for="item4"><p>This is my fourth item</p></label>
-  
+    <div className="todo-list"> 
+      <Todoitem/>
+      <Todoitem/>
+      <Todoitem/>
+      <Todoitem/>
     </div>
     )
-
-
-
 }
 
 
---
+++ Files/my-app/src/components/Todoitem.js
@@ -0,0 +1,14 @@
+import React from "react"
+import '../index.css'
+
+function Todoitem()
+{
+    return(
+    <div className="todo-item">
+        <input type="checkbox"/>
+        <p>Placeholder</p>
+    </div>)
+}
+
+
+export default Todoitem
