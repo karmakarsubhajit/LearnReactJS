1c40c8cddaa040b55e183c0c8e483c8740fd5501
111801041 <111801041@smail.iitpkd.ac.in>
2021-04-02 09:55:45 +0530

    inline style

--- Files/my-app/src/index.js
+++ Files/my-app/src/index.js
@@ -1,6 +1,25 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-ReactDOM.render(<div><h1>Hello World :)</h1> <h2>Bye World! :(</h2></div>,document.getElementById("root"))
+function App()
+{
+    const date = new Date()
+    const hours = date.getHours()
+    let greetings
+
+    if(hours<12)
+        greetings = "morning"
+    else if(hours>=12 && hours<17)
+        greetings = "afternoon"
+    else
+        greetings = "night"
+
+    const styles={color:"yellow",backgroundColor:"#12FE4A",fontSize:20}
+
+    return (<h1 style ={styles}>Good {greetings}</h1>)
+}
+
+
+ReactDOM.render(<App/>,document.getElementById("root"))
 
 
