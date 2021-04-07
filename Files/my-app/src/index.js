5b04febc02cd0157d76e626ea6923514bb2e4248
111801041 <111801041@smail.iitpkd.ac.in>
2021-04-07 14:19:48 +0530

    Class comp practice

--- Files/my-app/src/index.js
+++ Files/my-app/src/index.js
@@ -1,6 +1,52 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React, {Component} from "react"
 
-ReactDOM.render(<div><h1>Hello World :)</h1> <h2>Bye World! :(</h2></div>,document.getElementById("root"))
+import ReactDOM from "react-dom" 
+
+class App extends React.Component
+{
+    render(){
+        return(<div>
+            <Header username="Subhajit!"/>
+            <Greeting/>
+        </div>)
+    }
+} 
+
+class Header extends React.Component
+{
+    render(props)
+    {
+        return (<header>
+            <p>Welcome, {this.props.username}!</p>
+        </header>)
+    }
+}
+
+class Greeting extends React.Component
+{   
+    render()
+    {
+        const date =new Date()
+        const hours = date.getHours()
+        let timeOfDay
+        if(hours<12)
+        {
+            timeOfDay = "morning"
+        }
+        else if (hours>=12 && hours<17)
+        {
+            timeOfDay = "afternoon"
+        }
+        else{
+            timeOfDay="night"
+        }
+        return (<h1>Good {timeOfDay} to you, sir or madam</h1>)
+    }
+}
+
+
+
+
+ReactDOM.render(<App/>,document.getElementById("root"))
 
 
