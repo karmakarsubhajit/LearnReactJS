f7ae7fc7a701a8ec5e4eeb4dd5f8a1b0176d9c95
111801041 <111801041@smail.iitpkd.ac.in>
2021-04-07 16:10:22 +0530

    Change state

--- Files/my-app/src/App.js
+++ Files/my-app/src/App.js
@@ -1,23 +1,41 @@
 import React from "react"
 
-class App extends React.Component {
 
+class App extends React.Component
+{
   constructor()
   {
-      super()
-      this.state = {
-        answer:"Yes"
+    super()
+    {
+      this.state={
+        count: 0
       }
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick()
+  {
+    this.setState(prevState=>{
+        return {
+          count : prevState.count+1
+        }
+    })
+  }
+
+  render()
+  {
+    return(
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Change!</button>
+      </div>
+    )
+
   }
 
 
-  render(){
-      return (
-          <div>
-            <h1>Is state imp? {this.state.answer}</h1>
-          </div>
-      )
-  }
 }
 
 export default App
