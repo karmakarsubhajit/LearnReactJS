bc910c8575a137f984081b3fb0c25162362bd59d
111801041 <111801041@smail.iitpkd.ac.in>
2021-04-08 10:07:36 +0530

    change_state

--- Files/my-app/src/App.js
+++ Files/my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 
+
 class App extends React.Component
 {
   constructor()
@@ -8,34 +9,33 @@ class App extends React.Component
     super()
     {
       this.state={
-        count: 0
+        count:0
       }
-    }
-
+    }  
     this.handleClick = this.handleClick.bind(this)
+
   }
 
   handleClick()
   {
     this.setState(prevState=>{
-        return {
-          count : prevState.count+1
-        }
+      return {
+        count:prevState.count+1
+    }
     })
   }
 
   render()
   {
-    return(
-      <div>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.handleClick}>Change!</button>
-      </div>
-    )
-
+    return (<div>
+      <h1>{this.state.count}</h1>
+      <button onClick={this.handleClick}>Change!</button>
+    </div>)
   }
 
 
 }
+
+
 
 export default App
