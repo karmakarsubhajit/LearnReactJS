6e3d1b11e8437d0e1c6b712887f75cf887fcd8d5
111801041 <111801041@smail.iitpkd.ac.in>
2021-04-12 21:39:03 +0530

    todo phase 7

--- Files/my-app/src/components/Todoitem.js
+++ Files/my-app/src/components/Todoitem.js
@@ -2,14 +2,21 @@ import React from "react"
 import '../index.css'
 
 
+
 function Todoitem(props)
 {
+    const completedStyle ={
+        fontStyle:"italics",
+        color:"#cdefab",
+        textDecoration:"line-through"
+    }
+
+
     return(
     <div className="todo-item">
         <input onChange={()=>props.handleChange(props.todoItem.id)} type="checkbox" checked= {props.todoItem.completed}/>
-        <p>{props.todoItem.text}</p>
+        <p style={props.todoItem.completed?completedStyle:null}>{props.todoItem.text}</p>
     </div>)
 }
-
 
 export default Todoitem
