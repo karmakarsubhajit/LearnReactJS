9638c5d4cf976db8a732e6da3a2cc9d5569b5d01
111801041 <111801041@smail.iitpkd.ac.in>
2021-04-13 17:23:43 +0530

    Fetch API

--- Files/my-app/src/App.js
+++ Files/my-app/src/App.js
@@ -1,20 +1,37 @@
 import React from "react"
 
+
 class App extends React.Component {
 
   constructor()
   {
       super()
       this.state = {
-        answer:"Yes"
+        loading:false,
+        character:{}
       }
   }
 
-
+  componentDidMount()
+  {
+    this.setState({loading:true})
+    fetch("https:/swapi.dev/api/people/1")
+    .then(response =>response.json())
+    .then(data => {
+      this.setState({
+          loading:false,
+          character:data
+      })
+    })
+  }
+  
+  
   render(){
+
+      let loadText = this.state.loading?"Loading...":this.state.character.name
       return (
           <div>
-            <h1>Is state imp? {this.state.answer}</h1>
+            <h2>{loadText}</h2>
           </div>
       )
   }
